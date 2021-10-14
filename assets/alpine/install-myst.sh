168ca8b28c3136f5124cb011d9ae88b9792644be
Mantas Domaševičius <mantas@mysterium.network>
2021-10-14 16:44:20 +0300

    Myst install for alpine
    Signed-off-by: Mantas Domaševičius <mantas@mysterium.network>

---
+++ assets/alpine/install-myst.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+NODE_VERSION=$1
+
+if [ -z "$NODE_VERSION" ]; then
+        echo " > Missing NODE VERSION (i.e. 0.67.0)"
+        exit 1
+fi
+
+mkdir node
+cd node
+apk add --no-cache iptables ipset ca-certificates openvpn bash sudo openresolv
+wget https://raw.githubusercontent.com/mysteriumnetwork/node/master/bin/helpers/prepare-run-env.sh
+chmod +x ./prepare-run-env.sh
+./prepare-run-env.sh
+wget https://github.com/mysteriumnetwork/node/releases/download/${NODE_VERSION}/myst_linux_amd64.tar.gz
+tar -xvf myst_linux_amd64.tar.gz
+rm myst_linux_amd64.tar.gz
+cp myst /bin/
+wget https://raw.githubusercontent.com/mysteriumnetwork/hyperv-myst-provision/master/assets/alpine/myst-service
+mv myst-service /etc/init.d/
+chmod +x /etc/init.d/myst-service
+rc-update add myst-service default
+rc-service myst-service start
