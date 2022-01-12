c7bc0a73449ade8a63d44799a4745eb6131f5afe
Anton <jnashicq@gmail.com>
2022-01-12 16:23:39 +0300

    Fix a keystore sharing (p2)

--- assets/alpine/install-myst.sh
+++ assets/alpine/install-myst.sh
@@ -17,9 +17,9 @@ wget https://github.com/mysteriumnetwork/node/releases/download/${NODE_VERSION}/
 tar -xvf myst_linux_amd64.tar.gz
 rm myst_linux_amd64.tar.gz
 cp myst /bin/
-wget https://raw.githubusercontent.com/mysteriumnetwork/hyperv-myst-provision/master/assets/alpine/myst-service
-mv myst-service /etc/init.d/
-chmod +x /etc/init.d/myst-service
+#wget https://raw.githubusercontent.com/mysteriumnetwork/hyperv-myst-provision/master/assets/alpine/myst-service
+#mv myst-service /etc/init.d/
+#chmod +x /etc/init.d/myst-service
 #rc-update add myst-service default
 cd ..
 rm -rf node
