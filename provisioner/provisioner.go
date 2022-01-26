fa9d56bcdd49b8e160f75bcdf6a4b2d036083674
Anton <jnashicq@gmail.com>
2022-01-26 16:24:29 +0300

    Restore provisioner.go

---
+++ provisioner/provisioner.go
@@ -0,0 +1,23 @@
+package provisioner
+
+import "errors"
+
+func GetLatestNodeVersion(nodeVersion string) (string, error) {
+	if nodeVersion != "" {
+		_, err := gitRelease("mysteriumnetwork", "node", nodeVersion)
+		if err != nil {
+			return "", err
+		}
+		return nodeVersion, nil
+	}
+
+	releases, err := gitReleases("mysteriumnetwork", "node", 1)
+	if err != nil {
+		return "", err
+	}
+
+	if len(releases) == 0 {
+		return "", errors.New("did not find a single release of node")
+	}
+	return releases[0].TagName, nil
+}
